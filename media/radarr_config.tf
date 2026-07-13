aa8cb68ab7cdeefd94fb385497224675e349a780
jeelpatel231 <jeelpatel231@gmail.com>
2026-07-13 22:46:32 +0530

    add subsplease rss

--- media/radarr_config.tf
+++ media/radarr_config.tf
@@ -7,6 +7,19 @@ resource "radarr_root_folder" "root_folder" {
   }
 }
 
+resource "radarr_indexer_torrent_rss" "subsplease" {
+  name                    = "SubsPlease"
+  base_url                = "https://subsplease.org/rss/?t&r=1080"
+  allow_zero_size         = false
+  minimum_seeders         = 1
+  priority                = 1
+
+  depends_on = [terraform_data.wait_for_radarr_anime]
+  lifecycle {
+    replace_triggered_by = [docker_container.radarr_anime]
+  }
+}
+
 resource "radarr_download_client_qbittorrent" "qbit_client" {
   enable                     = true
   priority                   = 1
--- media/sonarr_config.tf
+++ media/sonarr_config.tf
@@ -7,6 +7,20 @@ resource "sonarr_root_folder" "root_folder" {
   }
 }
 
+resource "sonarr_indexer_torrent_rss" "subsplease" {
+  enable_automatic_search = true
+  name                    = "SubsPlease"
+  base_url                = "https://subsplease.org/rss/?t&r=1080"
+  allow_zero_size         = false
+  minimum_seeders         = 1
+  priority                = 1
+
+  depends_on = [terraform_data.wait_for_sonarr_anime]
+  lifecycle {
+    replace_triggered_by = [docker_container.sonarr_anime]
+  }
+}
+
 resource "sonarr_download_client_qbittorrent" "qbit_client" {
   enable                     = true
   priority                   = 1
